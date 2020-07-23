9c17d8d06257e3e758782365d8a77aae92a22000
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-07-23 10:36:42 -0400

    .

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-const makeDino = function() {
+
+const makeDino = function(species, period, diet, status = false){
+  const madeDino = {
+    species: species,
+    period: period,
+    carnivore: diet,
+    extinct: status,
+  };
+  return madeDino;
+
+};
+
+const makeSingular = function(obj){
+  const madeDinoObj = {
+  species: obj.species,
+  period: obj.period,
+  carnivore: obj.carnivore,
+  extinct: obj.extinct,
+};
+if(madeDinoObj.species.endsWith('us')){
+  madeDinoObj.species = madeDinoObj.species.slice(0,madeDinoObj.species.length-2)
+}else {
+  madeDinoObj.species = madeDinoObj.species}
+return madeDinoObj;
+
 }
 
-const makeSingular = function() {
-}
 
 const truncateSpecies = function() {
 }
